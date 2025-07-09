59a51f12f2de1efe1315965c57dc711a88960ed9
LILCODAH <rajkumar14ailawadi@gmail.com>
2025-07-09 19:08:37 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import MovieCard from './Components/MovieCards'
+
+function App() {
+  return (
+    <MovieCard movie={{title:"Inglorious Basterds" , release_date:"2009"}}/>
+  )
+}
+
+export default App
---
+++ src/Components/MovieCards.jsx
@@ -0,0 +1,19 @@
+export default function MovieCard({movie}){
+    const onFavouriteClick = ()=>{
+        alert("Clicked")
+    }
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favourite-btn" onClick={onFavouriteClick}>Click Me</button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    )
+}
